4bf4a46e4d81efe796467b6e70c417ca8e698ed4
541435721 <541435721@qq.com>
2016-07-12 10:07:58 +0800

    dadasd

---
+++ demo01/models.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+# __author__ = 'BianXuesheng'
+# __data__ = '2016/05/09_18:20 '
+
+from numpy import *
+
+
+class User(object):
+    def __init__(self, user_id, user_name):
+        self.user_id = user_id
+        self.user_name = user_name
+
+
+if __name__ == '__main__':
+    pass
